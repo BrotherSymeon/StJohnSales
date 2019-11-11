b7d108b69d8423e14f1c974f90b3b711e8c9bd88
Glitch (brothersymeon-stjohnsales-1) <none>
2019-11-11 19:00:06 +0000

    ✍️🍅 Checkpoint ./routes/index.js:7306958/30 ./views/about.hbs:7306958/153

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   try {
-    res.render('index', { title: 'Meadowlark Travel' });
+    res.render('index', { title: 'St John Sales' });
   } catch (err) {
     console.log(err);
     next(err);
