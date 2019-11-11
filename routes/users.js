396324c115a041173b385b1defe25ff21ab87e21
Glitch (brothersymeon-stjohnsales-1) <none>
2019-11-11 20:10:09 +0000

    ⛰🏓 Checkpoint ./routes/users.js:7306958/546

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "https://stjohnsales.glitch.me/users/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
