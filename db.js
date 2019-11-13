817242c164c9a387de7f7b05abde3d636cd86323
Glitch (stjohnsales) <none>
2019-11-13 12:07:13 +0000

    🏝🛳 Checkpoint ./db.js:7306958/296 ./bin/www:7306958/103

--- db.js
+++ db.js
@@ -28,7 +28,7 @@ exports.get = function() {
   return state.pool;
 };
 
-exports.fixtures = function(data) {
+exports.fixtures = function(data, done) {
   var pool = state.pool;
   if(!pool) return done(new Error('Missing database connection.'));
   
@@ -39,6 +39,14 @@ exports.fixtures = function(data) {
         ,values = keys.map(function(key){ return `'${row[key]}'`; });
       
       pool.query(`INSERT INTO ${name} ( ${ keys.join(',') } ) VALUES ( ${ values.join(',') } );`, cb);
-    });
-  });
+    }, cb);
+  }, done);
+};
+
+exports.drop = function(tables, done) {
+  var pool = state.pool;
+  if(!pool) return done(new Error('Missing database connection.'));
+  async.each(tables, function(name, cb) {
+    pool.query('DELETE * FROM ' + name, cb);
+  }, done);
 };
