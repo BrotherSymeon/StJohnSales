d387cdb197076bf22158e47a2e485cbb8b7ab7f2
Glitch (stjohnsales) <none>
2019-11-15 13:02:32 +0000

    🚄🏆 Checkpoint ./routes/index.js:7306958/2157

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,38 @@
-var express = require('express');
-var fortune = require('../lib/fortune');
+var express = require("express");
+var fortune = require("../lib/fortune");
 //var debug = require('debug')('meadowlark:server');
-
 
 var router = express.Router();
 
 var sessionChecker = (req, res, next) => {
-  console.log('sessionChecker')
-    if (req.session.user && req.cookies.user_sid) {
-        next();
-    } else {
-        res.redirect('/users/login');
-    }    
+  console.log("sessionChecker");
+  //console.log(req.session.user.id);
+  if (req.session.user && req.cookies.user_sid) {
+    next();
+  } else if (!req.session.user.id) {
+    res.redirect("/users/login");
+  } else {
+    res.redirect("/users/login");
+  }
 };
 
-
 /* GET home page. */
-router.get('/', sessionChecker, function (req, res, next) {
+router.get("/", sessionChecker, function(req, res, next) {
   try {
-    res.render('index', { title: 'St John Sales' });
+    res.render("index", { title: "St John Sales" });
   } catch (err) {
     console.log(err);
     next(err);
   }
-
 });
-router.get('/about',sessionChecker, function (req, res, next) {
-  //eq.cookies.user_sid && !req.session.user) 
+router.get("/about", sessionChecker, function(req, res, next) {
+  //eq.cookies.user_sid && !req.session.user)
   var randomFortune = fortune.getFortune();
-  res.render('about', {
+  res.render("about", {
     fortune: randomFortune,
-    title: 'Meadowlark Travel',
-    pageTestScript: '/qa/tests-about.js'
+    title: "Meadowlark Travel",
+    pageTestScript: "/qa/tests-about.js"
   });
 });
-
-
 
 module.exports = router;
