b7baacd2db044d10fa4b212b1a1272a9463d38b6
Glitch (stjohnsales) <none>
2019-11-17 18:33:08 +0000

    🖼🐿 Checkpoint ./controllers/adminController.js:7306958/395 ./routes/admin.js:7306958/1025

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,5 @@
 var User = require('../models/user');
-var Sales = require()
+var Sales = require('../models/sales');
 
 exports.users_list = function(req, res) {
    res.send('NOT IMPLEMENTED: page to list users');
@@ -13,14 +13,12 @@ exports.users_new = function(req, res) {
    res.send('NOT IMPLEMENTED: page to create new user');
 };
 
-exports.ordersdata_upload = function (req, res) {
-   res.send('NOT IMPLEMENTED: page to upload orders data');
+exports.user_create = function(req, res) {
+  // a function
+   res.send('NOT IMPLEMENTED: page to create new user');
 };
 
-exports.orderitems_upload = function (req, res) {
-   res.send('NOT IMPLEMENTED: page to upload order items data');
+exports.data_upload = function (req, res) {
+   res.send('NOT IMPLEMENTED: page to upload data');
 };
 
-exports.depositsdata_upload = function (req, res) {
-   res.send('NOT IMPLEMENTED: page to upload deposits data');
-};
--- routes/admin.js
+++ routes/admin.js
@@ -1,2 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var adminController = require('../controllers/adminController');
+
+
+
+
+router.get('/users', adminController.users_list);
+
+router.get('/users/:userId', adminController.users_detail);
+
+router.get('/users/new', adminController.users_new);
+
+router.get('/data', adminController.data_upload);
+
