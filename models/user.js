ae022c13a39dfdf4d85a62a4bd1441f08d554355
Glitch (stjohnsales) <none>
2019-11-17 18:55:50 +0000

    🚖🎙 Checkpoint ./models/user.js:7306958/3102

--- models/user.js
+++ models/user.js
@@ -1,47 +1,58 @@
-var db = require('../db');
+var db = require("../db");
 
-exports.create = function(username, email, description, done){
+exports.create = function(username, email, description, done) {
   var values = [username, email, description];
-  
-  db.get().query('INSERT INTO Users (username, email, description) VALUES(?, ?, ?)', values, function(err, result) {
-    if (err) return done(err);
-    done(null, result.insertId);
-  });
-  
+
+  db.get().query(
+    "INSERT INTO Users (username, email, description) VALUES(?, ?, ?)",
+    values,
+    function(err, result) {
+      if (err) return done(err);
+      done(null, result.insertId);
+    }
+  );
 };
 
 exports.getAll = function(done) {
-  db.get().query('SELECT * FROM Users;', function ( err, rows) {
+  db.get().query("SELECT * FROM Users;", function(err, rows) {
     if (err) return done(err);
-    done( null, rows);
+    done(null, rows);
   });
 };
 
-exports.create = function ({ username, email, description }, done) {
+exports.create = function({ username, email, description }, done) {
   let stmt = `INSERT INTO Users(username, email, description)
                 VALUES(?,?,?)`;
   let user = [username, email, description];
   db.get().query(stmt, user, function(err, results, fields) {
-    
-  })
-  
-}
-
-exports.findById = function (id, done) {
-   db.get().query('SELECT * FROM Users WHERE user_id = ?;', id, function ( err, rows) {
-    if (err) return done(err);
-    var user = JSON.parse(JSON.stringify(rows[0]));
-    done( null, user);
+    if (err) {
+      return console.error(err.message);
+    }
+    // get inserted id
+    console.log("User user_id:" + results.insertId);
   });
 };
 
-exports.findByEmail = function ( email, done ) {
-  db.get().query('SELECT * FROM Users WHERE email = ?;', email, function ( err, rows) {
+exports.findById = function(id, done) {
+  db.get().query("SELECT * FROM Users WHERE user_id = ?;", id, function(
+    err,
+    rows
+  ) {
+    if (err) return done(err);
+    var user = JSON.parse(JSON.stringify(rows[0]));
+    done(null, user);
+  });
+};
+
+exports.findByEmail = function(email, done) {
+  db.get().query("SELECT * FROM Users WHERE email = ?;", email, function(
+    err,
+    rows
+  ) {
     if (err) return done(err);
     console.log(err);
     //if (rows.length === 0) return done(new Error('no data'))
     console.log(rows);
-    done( null, rows);
+    done(null, rows);
   });
 };
-
