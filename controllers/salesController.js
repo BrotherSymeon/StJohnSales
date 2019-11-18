67a2194ef98544b6bf581b7112dccdc4101d2d7d
Glitch (stjohnsales) <none>
2019-11-18 13:39:27 +0000

    👞🚧 Checkpoint ./controllers/salesController.js:7306958/2

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -2,7 +2,7 @@ var Sales = require('../models/sales');
 
 exports.dashboard = async function(req, res) {
   
-  var data = await Sales.OrderAmountsByMonth(2019);
+  //var data = await Sales.OrderAmountsByMonth(2019);
   return res.render('index', { title : 'St Johns Sales'});
   
 };
