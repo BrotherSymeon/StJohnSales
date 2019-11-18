9f24f8c1934759afe410159598120c284a25837f
Glitch (stjohnsales) <none>
2019-11-18 19:39:16 +0000

    🐡💗 Checkpoint ./models/sales.js:7306958/688 ./controllers/salesController.js:7306958/105

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -4,8 +4,10 @@ var someAsync = require('../helpers/someAsync');
 exports.dashboard = async function(req, res) {
   try{
     var data = await Sales.OrderAmountsByMonth(2019);
+    var quarterData = await Sales.OrderAmountsByQuarter(2019);
     //console.log(data)
     res.locals.data = data;
+    res.locals.quarterData = quarterData;
     return res.render('index', { title : 'St Johns Sales'});
   }catch(err){
     console.error(err);
--- models/sales.js
+++ models/sales.js
@@ -14,3 +14,15 @@ exports.OrderAmountsByMonth = async function ( year ) {
  
 };
 
+exports.OrderAmountsByQuarter = async function( year ) {
+  //SELECT MonthOfYear, SUM(OrderTotal) as Amount FROM Orders WHERE SaleYear=? GROUP BY MonthOfYear;
+    return new Promise(function(resolve, reject){
+    salesDb.connect(salesDb.MODE_PROD, function(){
+      salesDb.get().query(' SELECT QuarterOfYear, SUM(OrderTotal) as Amount FROM Orders GROUP BY QuarterOfYear;', year, function(err, result) {
+        if (err) return reject(err);
+        resolve(JSON.parse(JSON.stringify(result)));
+      });
+    })
+   
+  });
+}
