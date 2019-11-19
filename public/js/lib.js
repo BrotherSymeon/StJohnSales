e64c3304e7410011deac4f63995f593824d3c75c
Glitch (stjohnsales) <none>
2019-11-19 20:08:21 +0000

    🏨👸 Checkpoint ./views/index.hbs:7306958/541 ./public/js/lib.js:7306958/419

---
+++ public/js/lib.js
@@ -0,0 +1,13 @@
+  function convertHex (hex, opacity) {
+    hex = hex.replace('#', '');
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    const result = 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')';
+    return result;
+  }
+
+  function random (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
