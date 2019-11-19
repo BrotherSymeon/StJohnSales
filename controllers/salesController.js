cbdaaccfea25bc8b0278f800b9b51e9850ea7550
Glitch (stjohnsales) <none>
2019-11-19 16:02:19 +0000

    🎱☄️ Checkpoint ./views/index.hbs:7306958/465 ./controllers/salesController.js:7306958/241

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -3,13 +3,16 @@ var someAsync = require('../helpers/someAsync');
 
 exports.dashboard = async function(req, res) {
   try{
-    var data = await Sales.OrderAmountsByMonth(2019, 'ETSY');
-    var quarterData = await Sales.OrderAmountsByQuarter(2019, 'ETSY');
-    var squareData = await Sales.OrderAmountsByMonth(2019, 'SQUARE');
-    var squareQuarterData = await Sales.OrderAmountsByQuarter(2019, 'SQUARE');
+    var etsyMonthlyData = await Sales.OrderAmountsByMonth(2019, 'ETSY');
+    var etsyQuarterlyData = await Sales.OrderAmountsByQuarter(2019, 'ETSY');
+    var squareMonthlyData = await Sales.OrderAmountsByMonth(2019, 'SQUARE');
+    var squareQuarterlyData = await Sales.OrderAmountsByQuarter(2019, 'SQUARE');
     //console.log(data)
-    res.locals.data = data;
-    res.locals.quarterData = quarterData;
+    res.locals.etsyMonthlyData = etsyMonthlyData;
+    res.locals.etsyQuarterlyData = etsyQuarterlyData;
+    res.locals.squareMonthlyData = squareMonthlyData;
+    res.locals.squareQuarterlyData = squareQuarterlyData;
+    
     return res.render('index', { title : 'St Johns Sales'});
   }catch(err){
     console.error(err);
