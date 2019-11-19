3a21a9b6a41d0da16217fe404deb3b637c774c3b
Glitch (stjohnsales) <none>
2019-11-19 15:26:19 +0000

    🏫💠 Checkpoint ./models/sales.js:7306958/89

--- models/sales.js
+++ models/sales.js
@@ -4,11 +4,14 @@ var salesDb = require('../gcpDb');
 exports.OrderAmountsByMonth = async function ( year ) {
   return new Promise(function(resolve, reject){
     salesDb.connect(salesDb.MODE_PROD, function(){
-      salesDb.get().query('SELECT MonthOfYear, SUM(OrderTotal) as Amount FROM Orders GROUP BY MonthOfYear', year, function(err, result) {
+      salesDb.get().query('SELECT MonthOfYear, SUM(OrderTotal) as Amount FROM Orders WHERE PaymentMethod != "Other" GROUP BY MonthOfYear', year, function(err, result) {
         if (err) return reject(err);
         resolve(JSON.parse(JSON.stringify(result)));
       });
     })
+    
+    
+    
    
   });
  
@@ -18,7 +21,7 @@ exports.OrderAmountsByQuarter = async function( year ) {
   //SELECT MonthOfYear, SUM(OrderTotal) as Amount FROM Orders WHERE SaleYear=? GROUP BY MonthOfYear;
     return new Promise(function(resolve, reject){
     salesDb.connect(salesDb.MODE_PROD, function(){
-      salesDb.get().query(' SELECT QuarterOfYear, SUM(OrderTotal) as Amount FROM Orders GROUP BY QuarterOfYear;', year, function(err, result) {
+      salesDb.get().query(' SELECT QuarterOfYear, SUM(OrderTotal) as Amount FROM Orders WHERE PaymentMethod != "Other" GROUP BY QuarterOfYear;', year, function(err, result) {
         if (err) return reject(err);
         resolve(JSON.parse(JSON.stringify(result)));
       });
