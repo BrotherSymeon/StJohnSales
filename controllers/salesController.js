5304e96b4d21fd3019f5bf6eff2f6b3f5afa3e5f
Glitch (stjohnsales) <none>
2019-11-19 15:50:20 +0000

    🌛🏫 Checkpoint ./controllers/salesController.js:7306958/169 ./models/sales.js:7306958/133 ./views/index.hbs:7306958/374

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -5,6 +5,8 @@ exports.dashboard = async function(req, res) {
   try{
     var data = await Sales.OrderAmountsByMonth(2019, 'ETSY');
     var quarterData = await Sales.OrderAmountsByQuarter(2019, 'ETSY');
+    var squareData = await Sales.OrderAmountsByMonth(2019, 'SQUARE');
+    var squareQuarterData = await Sales.OrderAmountsByQuarter(2019, 'SQUARE');
     //console.log(data)
     res.locals.data = data;
     res.locals.quarterData = quarterData;
--- models/sales.js
+++ models/sales.js
@@ -2,14 +2,14 @@ var salesDb = require('../gcpDb');
 
 
 exports.OrderAmountsByMonth = async function ( year, soldThrough ) {
-  var params = [year, soldThrough]
+  var params = [year, soldThrough];
   return new Promise(function(resolve, reject){
     salesDb.connect(salesDb.MODE_PROD, function(){
       salesDb.get().query('SELECT MonthOfYear, SUM(OrderTotal) as Amount FROM Orders WHERE SaleYear = ? AND SoldThrough = ? GROUP BY MonthOfYear', params, function(err, result) {
         if (err) return reject(err);
         resolve(JSON.parse(JSON.stringify(result)));
       });
-    })
+    });
     
     
     
@@ -18,15 +18,16 @@ exports.OrderAmountsByMonth = async function ( year, soldThrough ) {
  
 };
 
-exports.OrderAmountsByQuarter = async function( year ) {
+exports.OrderAmountsByQuarter = async function( year, soldThrough ) {
   //SELECT MonthOfYear, SUM(OrderTotal) as Amount FROM Orders WHERE SaleYear=? GROUP BY MonthOfYear;
+  var params = [year, soldThrough];
     return new Promise(function(resolve, reject){
     salesDb.connect(salesDb.MODE_PROD, function(){
-      salesDb.get().query(' SELECT QuarterOfYear, SUM(OrderTotal) as Amount FROM Orders WHERE PaymentMethod != "Other" GROUP BY QuarterOfYear;', year, function(err, result) {
+      salesDb.get().query(' SELECT QuarterOfYear, SUM(OrderTotal) as Amount FROM Orders WHERE SaleYear = ? AND SoldThrough = ? GROUP BY QuarterOfYear;', params, function(err, result) {
         if (err) return reject(err);
         resolve(JSON.parse(JSON.stringify(result)));
       });
-    })
+    });
    
   });
-}
+};
