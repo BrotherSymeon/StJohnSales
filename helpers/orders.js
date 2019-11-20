0bac25e54e6a8b6d5f2ee4abf48fa636f3cca28a
Glitch (stjohnsales) <none>
2019-11-20 19:13:52 +0000

    👣🚀 Checkpoint ./helpers/orders.js:7306958/3671

--- helpers/orders.js
+++ helpers/orders.js
@@ -49,51 +49,55 @@ let OrderColumns = new Enumeration([
 
 exports.process = function (data, {processId, fileName}) {
   var e = new emitter();
-  let retval = [];
-  const lines = data.split('\n');
-  e.emit('BeginProcess', {
-    processId: processId,
-    message: `Processing ${fileName}: ${lines.length} lines of data to process.`,
-    percentDone: 0
-  });
-  lines.forEach((val, i, array) => {
-     e.emit('BeginProcess', {
-    processId: processId,
-    message: `Processing ${fileName}: ${lines.length} lines of data to process.`,
-    percentDone: 0
-  });
-    //val is a comma seperated line
-    val = utils.replaceCommasInDoubleQuotes(val)
-    if (i > 0) {
-      let lineData = val.split(',');
-     
-      lineData.forEach((elem, index) => {
-
-        if ([OrderColumns.ADJUSTEDCARDPROCESSINGFEES,
-        OrderColumns.ADJUSTEDNETORDERAMOUNT,
-        OrderColumns.CARDPROCESSINGFEES,
-        OrderColumns.INPERSONDISCOUNT,
-        OrderColumns.ADJUSTEDORDERTOTAL,
-        OrderColumns.ORDERTOTAL,
-        OrderColumns.SHIPPING,
-        OrderColumns.SHIPPINGDISCOUNT,
-        OrderColumns.SALESTAX,
-        OrderColumns.DISCOUNTAMOUNT,
-        OrderColumns.ORDERVALUE,
-        OrderColumns.ORDERNET].includes(index)) {
-          console.log(utils.removeCharacters( elem, '"' ));
-          lineData[index] = Number(utils.removeCharacters( elem, '"' ));
-        }else{
-          lineData[index] = utils.removeCharacters( elem, '"' );
-        }
-
+  setTimeout(function(){
+    
+    let cleanData = [];
+    const lines = data.split('\n');
+    e.emit('BeginFileProcess', {
+      processId: processId,
+      message: `Processing ${fileName}: ${lines.length} lines of data to process.`,
+      percentDone: 0
+    });
+    lines.forEach((val, i, array) => {
+      e.emit('FileLineProcess', {
+        processId: processId,
+        message: `Processing ${fileName}: processing line ${i} of ${lines.length} lines.`,
+        percentDone: ((i / lines.length) * 100)
       });
-      retval.push( lineData.join('\t') );
-    }
+      //val is a comma seperated line
+      val = utils.replaceCommasInDoubleQuotes(val);
+      if (i > 0) {
+        let lineData = val.split(',');
+
+        lineData.forEach((elem, index) => {
+
+          if ([OrderColumns.ADJUSTEDCARDPROCESSINGFEES,
+          OrderColumns.ADJUSTEDNETORDERAMOUNT,
+          OrderColumns.CARDPROCESSINGFEES,
+          OrderColumns.INPERSONDISCOUNT,
+          OrderColumns.ADJUSTEDORDERTOTAL,
+          OrderColumns.ORDERTOTAL,
+          OrderColumns.SHIPPING,
+          OrderColumns.SHIPPINGDISCOUNT,
+          OrderColumns.SALESTAX,
+          OrderColumns.DISCOUNTAMOUNT,
+          OrderColumns.ORDERVALUE,
+          OrderColumns.ORDERNET].includes(index)) {
+            console.log(utils.removeCharacters( elem, '"' ));
+            lineData[index] = Number(utils.removeCharacters( elem, '"' ));
+          }else{
+            lineData[index] = utils.removeCharacters( elem, '"' );
+          }
+        });
+        cleanData.push( lineData.join('\t') );
+      }
+    });
 
 
-  });
+    }, 2000);
+  
 
+  
   return e;
 
 };
