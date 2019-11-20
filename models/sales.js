3dd485fcc402242a104f1afcb7e9cd86739e57da
Glitch (stjohnsales) <none>
2019-11-20 19:26:30 +0000

    🙅🏟 Checkpoint ./models/sales.js:7306958/1524

--- models/sales.js
+++ models/sales.js
@@ -1,6 +1,46 @@
 var salesDb = require('../gcpDb');
 
-
+exports.InsertIntoOrderTable = async function (lines) {
+  var rows = lines.map((line) => line.split('\t'))
+  var sqlStmt = `INSERT INTO tempOrders(  
+  SaleDate VARCHAR(8) N,
+  OrderId INT UNSIGNED N,
+  PRIMARY KEY (O,
+  BuyerUserId VARCH,
+  FullName VARC,
+  FirstName VARC,
+  LastName VARC,
+  NumberOfItems INT UNSIGNED,
+  PaymentMethod VARC,
+  DateShipped VAR,
+  Street1 VARC,
+  Street2 VARC,
+  ShipCity VARC,
+  ShipState VARC,
+  ShipZipCode VARC,
+  ShipCountry VARC,
+  Currency VARCHAR(10),
+  OrderValue DECIMAL(6,2) DEFAULT 0.0,
+  CouponCode VARCHAR(20),
+  CouponDetails VARCHAR(20),
+  DiscountAmount DECIMAL(6,2) DEFAULT 0.0,
+  ShippingDiscount DECIMAL(6,2) DEFAULT 0.0,
+  Shipping DECIMAL(6,2) DEFAULT 0.0,
+  SalesTax DECIMAL(6,2) DEFAULT 0.0,
+  OrderTotal DECIMAL(6,2) DEFAULT 0.0,
+  Status VARCHAR(20),
+  CardProcessingFees DECIMAL(6,2) DEFAULT 0.0,
+  OrderNet DECIMAL(6,2) DEFAULT 0.0,
+  AdjustedOrderTotal DECIMAL(6,2) DEFAULT 0.00 NULL,
+  AdjustedCardProcessingFees DECIMAL(6,2) DEFAULT 0.0,
+  AdjustedNetOrderAmount DECIMAL(6,2) DEFAULT 0.0,
+  Buyer VARCHAR(50),
+  OrderType VARCHAR(50),
+  PaymentType VARCHAR(50),
+  InPersonDiscount DECIMAL(6,2) DEFAULT 0.0,
+  InPersonLocation VARCHAR(50))  VALUES ?  `;
+  
+};
 exports.OrderAmountsByMonth = async function ( year, soldThrough ) {
   var params = [year, soldThrough];
   return new Promise(function(resolve, reject){
