4871fe942d9844d896563a998d5a1e98deb5d23d
Glitch (stjohnsales) <none>
2019-11-25 13:10:29 +0000

    🐈🎮 Checkpoint ./models/sales.js:7306958/360

--- models/sales.js
+++ models/sales.js
@@ -39,50 +39,58 @@ exports.InsertIntoOrderTable = async function (lines, emitter) {
               PaymentType,
               InPersonDiscount,
               InPersonLocation)  VALUES ?  `;
-  var promises = [];
-  promises.push(new Promise(function(resolve, reject){
-    salesDb.get().query(sqlDeleteStmt, function(err, result){
-      if (err) return reject(err);
-      resolve(result.affectedRows);
-    });
-  }));
-  console.log(`line count = ${rows[0].length}`);
-  console.log(rows[0]);
-  promises.concat(rows.map(function(line) {
-    
-    return new Promise(function(resolve, reject){
-      var query = salesDb.get().query(sqlInsertStmt, line, function(err, result) {
+  
+  console.log([[rows[0]]])
+  var query = salesDb.get().query(sqlInsertStmt, [[rows[0]]], function(err, result) {
         if (err) return reject(err);
         resolve(result.affectedRows);
       });
-      console.log(query.sql)
-    });
-  }));
+  console.log(query.sql);
+    
+  //var promises = [];
+  //promises.push(new Promise(function(resolve, reject){
+  //  salesDb.get().query(sqlDeleteStmt, function(err, result){
+  //    if (err) return reject(err);
+  //    resolve(result.affectedRows);
+  //  });
+  //}));
+  //console.log(`line count = ${rows[0].length}`);
+  //console.log(rows[0]);
+  //promises.concat(rows.map(function(line) {
+    
+  //  return new Promise(function(resolve, reject){
+  //    var query = salesDb.get().query(sqlInsertStmt, line, function(err, result) {
+  //      if (err) return reject(err);
+  //      resolve(result.affectedRows);
+  //    });
+  //    console.log(query.sql)
+  //  });
+  //}));
   
-  return new Promise(function(resolve, reject){
-    salesDb.connect(salesDb.MODE_PROD, function(){
+  //return new Promise(function(resolve, reject){
+  //  salesDb.connect(salesDb.MODE_PROD, function(){
       
-      promises.reduce((promiseChain, currentArray) => {
-        return promiseChain.then(chainResults =>
-                Promise.all(currentArray).then(currentResult =>
-                    [...chainResults, currentResult]
-                )
-            );
-        }, Promise.resolve([])).then(arrayOfArraysOfResults => {
+  //    promises.reduce((promiseChain, currentArray) => {
+  //      return promiseChain.then(chainResults =>
+  //              Promise.all(currentArray).then(currentResult =>
+  //                  [...chainResults, currentResult]
+  //              )
+  //          );
+  //      }, Promise.resolve([])).then(arrayOfArraysOfResults => {
             // Do something with all results
-            console.log(arrayOfArraysOfResults);
-            resolve(arrayOfArraysOfResults);
-      }).catch(function(err){
-        console.log(err)
-      });
+  //          console.log(arrayOfArraysOfResults);
+  //          resolve(arrayOfArraysOfResults);
+  //    }).catch(function(err){
+  //      console.log(err)
+  //    });
       
       //Promise.all(promises).then(values => { 
       //  console.log(values); // [3, 1337, "foo"] 
       //  resolve(values)
       //});
       
-    });
-  });
+   // });
+  //});
   
 };
 exports.OrderAmountsByMonth = async function ( year, soldThrough ) {
