680c637dac43808a15c6f2cc29505a8655a326d4
Glitch (stjohnsales) <none>
2019-11-25 16:54:41 +0000

    💕👔 Checkpoint ./models/sales.js:7306958/3353

--- models/sales.js
+++ models/sales.js
@@ -5,14 +5,6 @@ exports.InsertIntoOrderTable = async function (lines, emitter) {
   var sqlDeleteStmt = 'DELETE FROM tempOrders;';
   var rows = lines;
   var sqlInsertStmt = 'INSERT INTO tempOrders(SaleDate,OrderId,BuyerUserId,FullName,FirstName,LastName,NumberOfItems,PaymentMethod,DateShipped,Street1,Street2,ShipCity,ShipState,ShipZipCode,ShipCountry,Currency,OrderValue,CouponCode,CouponDetails,DiscountAmount,ShippingDiscount,Shipping,SalesTax,OrderTotal,Status,CardProcessingFees,OrderNet,AdjustedOrderTotal,AdjustedCardProcessingFees,AdjustedNetOrderAmount,Buyer,OrderType,PaymentType,InPersonDiscount,InPersonLocation)  VALUES ?';
-  //console.log('first  row');
-  //console.log([[rows[0]]]);
-  //var query = salesDb.get().query(sqlInsertStmt, [[rows[0]]], function(err, result) {
-  //      if (err) return console.log(err);
-  //      return Promise.resolve(result.affectedRows);
-  //    });
-  //console.log(query.sql);
-    
   var promises = [];
   
   promises.push(function(done){
@@ -23,19 +15,21 @@ exports.InsertIntoOrderTable = async function (lines, emitter) {
   });
   
   rows.forEach((line => {
-    promises.push()
-  }))
-  promises.concat(rows.map(function(line) {
-    
-    return function insert(done) {
+    promises.push(function(done) {
         salesDb.get().query(sqlInsertStmt, [[line]], (err, result) => {
           if (err) return done(err);
           return done(null, result);
         });
-    };
-    
-    
+    });
   }));
+  
+  promises.push(function(done){
+    salesDb.get().query(extractToOrders, (err, result) => {
+      if (err) return done(err);
+      return done(null, result.affectedRows);
+    });
+  });
+ 
   
   
   console.log(promises.length)
@@ -52,22 +46,6 @@ exports.InsertIntoOrderTable = async function (lines, emitter) {
   
    
   
-  //return new Promise(function(resolve, reject){
-  //  salesDb.connect(salesDb.MODE_PROD, function(){
-  //    promises.reduce((promiseChain, currentArray) => {
-  //      console.log(currentArray);
-  //      return promiseChain.then(chainResults =>
-  //              Promise.all(currentArray).then(currentResult => currentResult)
-  //          );
-  //      }, Promise.resolve([])).then(arrayOfArraysOfResults => {
-  //          // Do something with all results
-  //          console.log(arrayOfArraysOfResults);
-  //          resolve(arrayOfArraysOfResults);
-  //    }).catch(function(err){
-  //      console.log(err);
-  //    });
-  //  });
-  //});
   
 };
 exports.OrderAmountsByMonth = async function ( year, soldThrough ) {
@@ -100,3 +78,103 @@ exports.OrderAmountsByQuarter = async function( year, soldThrough ) {
    
   });
 };
+
+
+
+const extractToOrders = `INSERT INTO Orders (
+  SaleDate,
+  DayOfYear, 
+  MonthOfYear, 
+  QuarterOfYear, 
+  SaleYear,
+  SoldThrough ,
+  BuyerUserId ,
+  FullName,
+  FirstName ,
+  LastName ,
+  NumberOfItems,
+  PaymentMethod ,
+  DateShipped,
+  Street1 ,
+  Street2 ,
+  ShipCity ,
+  ShipState ,
+  ShipZipCode ,
+  ShipCountry ,
+  Currency ,
+  OrderValue ,
+  CouponCode ,
+  CouponDetails ,
+  DiscountAmount,
+  ShippingDiscount,
+  Shipping,
+  SalesTax,
+  OrderTotal,
+  Status,
+  CardProcessingFees,
+  OrderNet,
+  AdjustedOrderTotal,
+  AdjustedCardProcessingFees,
+  AdjustedNetOrderAmount,
+  Buyer ,
+  OrderType ,
+  PaymentType ,
+  InPersonDiscount ,
+  InPersonLocation 
+) select
+  SaleDate,
+  DAYOFYEAR(DATE( 
+    CONCAT( 
+      CONCAT('20', MID(SaleDate, 7, 2)), 
+      '-', 
+      MID(SaleDate, 1, 2),
+      '-', 
+      MID(SaleDate, 4, 2) 
+      ) 
+    )) as DayOfYear,
+  MID(SaleDate, 1, 2) as SaleMonth,  
+    FiscalYearStartDate( DATE( 
+    CONCAT( 
+      CONCAT('20', MID(SaleDate, 7, 2)), 
+      '-', 
+      MID(SaleDate, 1, 2),
+      '-', 
+      MID(SaleDate, 4, 2) 
+      ) 
+    )) as Quarter,
+    CONCAT('20', MID(SaleDate, 7, 2)),
+    IF(PaymentMethod = 'Other', 'SQUARE','ETSY'),
+     BuyerUserId ,
+  FullName,
+  FirstName ,
+  LastName ,
+  NumberOfItems,
+  PaymentMethod ,
+  DateShipped,
+  Street1 ,
+  Street2 ,
+  ShipCity ,
+  ShipState ,
+  ShipZipCode ,
+  ShipCountry ,
+  Currency ,
+  OrderValue ,
+  CouponCode ,
+  CouponDetails ,
+  DiscountAmount,
+  ShippingDiscount,
+  Shipping,
+  SalesTax,
+  OrderTotal,
+  Status,
+  CardProcessingFees,
+  OrderNet,
+  AdjustedOrderTotal,
+  AdjustedCardProcessingFees,
+  AdjustedNetOrderAmount,
+  Buyer ,
+  OrderType ,
+  PaymentType ,
+  InPersonDiscount ,
+  InPersonLocation 
+  FROM tempOrders;`;
