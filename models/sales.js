db066945f4cdb9e8c3c49c980bc68992e128261f
Glitch (stjohnsales) <none>
2019-11-25 19:35:28 +0000

    🍉😎 Checkpoint ./models/sales.js:7306958/155

--- models/sales.js
+++ models/sales.js
@@ -6,6 +6,7 @@ exports.InsertIntoOrderTable = async function (lines, emitter) {
   var rows = lines;
   var sqlInsertStmt = 'INSERT INTO tempOrders(SaleDate,OrderId,BuyerUserId,FullName,FirstName,LastName,NumberOfItems,PaymentMethod,DateShipped,Street1,Street2,ShipCity,ShipState,ShipZipCode,ShipCountry,Currency,OrderValue,CouponCode,CouponDetails,DiscountAmount,ShippingDiscount,Shipping,SalesTax,OrderTotal,Status,CardProcessingFees,OrderNet,AdjustedOrderTotal,AdjustedCardProcessingFees,AdjustedNetOrderAmount,Buyer,OrderType,PaymentType,InPersonDiscount,InPersonLocation)  VALUES ?';
   var promises = [];
+  var errors = [];
   
   promises.push(function(done){
     salesDb.get().query(sqlDeleteStmt, (err, result) => {
@@ -17,7 +18,10 @@ exports.InsertIntoOrderTable = async function (lines, emitter) {
   rows.forEach((line => {
     promises.push(function(done) {
         salesDb.get().query(sqlInsertStmt, [[line]], (err, result) => {
-          if (err) return done(null, err);
+          if (err) {
+            errors.push({ err, result})
+            return done(null, err);
+          }
           return done(null, result);
         });
     });
@@ -37,9 +41,10 @@ exports.InsertIntoOrderTable = async function (lines, emitter) {
     
     async.series(promises, (err, results) => {
         if (err) {
-            return console.log(err);
+            console.log(err);
         }
-        return console.log(results);
+        console.log(errors)
+        //console.log(results);
     });
     
   });
