e2c0b270240abf6679b11e31cbcfd903c06ebaa5
Glitch (stjohnsales) <none>
2019-11-27 17:08:17 +0000

    🏩🎠 Checkpoint ./controllers/fileProcessController.js:7306958/654 ./views/upload.hbs:7306958/240

--- controllers/fileProcessController.js
+++ controllers/fileProcessController.js
@@ -2,10 +2,11 @@ let FileProcess = require('../models/FileProcess');
 let FileProcessDetails = require('../models/FileProcessDetails');
 
 exports.process_status = function(req, res){
+  var sql = ` SELECT * from  FileProcessDetails  where FileId = ${req.params.id} and FileProcessDetailId = (   select max(FileProcessDetailId) from FileProcessDetails WHERE FileId = ${req.params.id}  );`
   //console.log(req.params.id);
   var detail = new FileProcessDetails();
-  detail.find('all', {where: 'FileId ='+req.params.id}, function(err, rows, fields){
-    
+  detail.query(sql, function(err, rows, fields){
+    res.json(rows);
   })
-  res.json(req.params.id);
+  //res.json(req.params.id);
 };
