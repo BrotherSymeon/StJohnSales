9d5c09e6de4263c2c002f912aac3f3fd2b7b8321
Glitch (stjohnsales) <none>
2019-11-27 16:44:18 +0000

    🤘😬 Checkpoint ./controllers/fileProcessController.js:7306958/16

--- controllers/fileProcessController.js
+++ controllers/fileProcessController.js
@@ -2,6 +2,6 @@ let FileProcess = require('../models/FileProcess');
 let FileProcessDetails = require('../models/FileProcessDetails');
 
 exports.process_status = function(req, res){
-  console.log(req.params.id);
-  res.JSON(req.params.id);
+  //console.log(req.params.id);
+  res.JSON(2);
 };
