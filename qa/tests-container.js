0caf763ad87c7d907df47beecf2ee62e8a521e87
fr.Innocent <brother.symeon@gmail.com>
2019-11-29 09:53:03 -0500

    added test for container

--- qa/tests-container.js
+++ qa/tests-container.js
@@ -5,11 +5,11 @@ var expect = require('chai').expect;
 suite('diContainer', function(){
   test(' should return an object', function(){
     expect(typeof diContainer === 'object');
-  });
-  test('should get previously registered value', function(){
+  }); 
+  test(' should get previously registered value', function(){
     diContainer.register('dbName', 'example-db');
     const actual = diContainer.get('dbName');
     const expected = 'example-db';
     expect(actual === expected);
-  })
+  });
 });
