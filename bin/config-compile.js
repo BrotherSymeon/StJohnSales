1bcf1c12bf2847ada1dfdfeae6af682ce3aaa3ec
fr.Innocent <brother.symeon@gmail.com>
2020-03-17 15:50:12 -0400

    fixed file names

--- bin/config-compile.js
+++ bin/config-compile.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-const template = fs.readFileSync('app.tpl.yaml').toString();
+const template = fs.readFileSync('../app.tpl.yaml').toString();
 const content = ejs.render(template, process.env);
 
-fs.writeFileSync('app.yaml', content);
+fs.writeFileSync('../app.yml', content);
