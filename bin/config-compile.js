813298d14fe09ce934109901bbe620948803aaea
fr.Innocent <brother.symeon@gmail.com>
2020-03-17 16:50:54 -0400

    added console log

--- bin/config-compile.js
+++ bin/config-compile.js
@@ -7,6 +7,7 @@ const content = ejs.render(template, process.env);
 
 try {
   fs.writeFileSync('app.yaml', content);
+  console.log('app.yaml written successfully');
 } catch (e) {
   console.error(`${JSON.stringify(e)}`);
 };
