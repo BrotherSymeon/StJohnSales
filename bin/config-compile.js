b794b3aadffb0990f95cac55b12b0c52f7709710
fr.Innocent <brother.symeon@gmail.com>
2020-03-17 16:03:47 -0400

    changed command

--- bin/config-compile.js
+++ bin/config-compile.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-const template = fs.readFileSync('../app.tpl.yaml').toString();
+const template = fs.readFileSync('app.tpl.yaml').toString();
 const content = ejs.render(template, process.env);
 
-fs.writeFileSync('../app.yml', content);
+fs.writeFileSync('app.yml', content);
