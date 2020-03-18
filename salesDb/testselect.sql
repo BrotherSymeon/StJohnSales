67a0768fa7bc51c4342e78fbd15cd5c50a768196
fr.Innocent <brother.symeon@gmail.com>
2020-03-18 16:29:58 -0400

    Added a deploy_db to gitlab-ci.yaml

--- salesDb/testselect.sql
+++ salesDb/testselect.sql
@@ -1,7 +1,1 @@
- SELECT pd.*, fp.ProcessStatus from
-      FileProcessDetails pd
-      INNER JOIN FileProcess fp
-        ON fp.FileId = pd.FileId
-      where fp.FileId = 3 and
-      FileProcessDetailId = (   select max(FileProcessDetailId) from FileProcessDetails WHERE pd.FileId = 3  );
-
+SELECT "hello world" from dual;
